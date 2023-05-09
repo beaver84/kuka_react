c79818b4d6f0615919d453ac7f0e2aa6ba00c2e7
baekyungtae <>
2023-05-09 16:09:06 +0900

    배열에 데이터 삽입하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React, {Component} from 'react';
 import PhoneForm from "./component/PhoneForm";
 
-function App() {
-  return (
-    <div>
-      <PhoneForm />
-    </div>
-  );
+
+
+class App extends Component {
+
+    id = 0;
+
+    state = {
+        information: [],
+    }
+    handleCreate = (data) => {
+        const { information } = this.state;
+        this.setState({
+            information: information.concat({
+                ...data,
+                id: this.id++
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <PhoneForm onCreate={this.handleCreate}/>
+                {JSON.stringify(this.state.information)}
+            </div>
+        );
+    }
 }
 
 export default App;
