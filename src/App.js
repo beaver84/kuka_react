fe935844b99bd0d4937db172380810f426a3b30a
baekyungtae <>
2023-05-09 18:46:57 +0900

    검색 기능까지 추가

--- src/App.js
+++ src/App.js
@@ -6,10 +6,33 @@ import PhoneInfoList from "./component/PhoneInfoList";
 
 class App extends Component {
 
-    id = 0;
+    id = 3;
 
     state = {
-        information: [],
+        information: [
+            {
+                id: 0,
+                name: '배경태',
+                phone: '010-0000-0001'
+            },
+            {
+                id: 1,
+                name: '배이든',
+                phone: '010-0000-0002'
+            },
+            {
+                id: 2,
+                name: '알쭝이',
+                phone: '010-0000-0003'
+            }
+        ],
+        keyword:'',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            keyword: e.target.value,
+        })
     }
     handleCreate = (data) => {
         const { information } = this.state;
@@ -49,8 +72,15 @@ class App extends Component {
         return (
             <div>
                 <PhoneForm onCreate={this.handleCreate}/>
+                <input
+                    value={this.state.keyword}
+                    onChange={this.handleChange}
+                    placeholder="검색..."
+                />
                 <PhoneInfoList
-                    data={this.state.information}
+                    data={this.state.information.filter(
+                        info => info.name.indexOf(this.state.keyword) > -1
+                    )}
                     onRemove={this.handleRemove}
                     onUpdate={this.handleUpdate}
                 />
